923ab40bfdec40f5ef748913c3f3440701c70314
Vitor Lucas <vitorlucasdbr13@gmail.com>
2025-10-24 15:38:10 -0300

    refactor: mudando de banco local pra PostgreSQL

--- src/main/java/com/example/Primeiro_Projeto/PrimeiroProjetoApplication.java
+++ src/main/java/com/example/Primeiro_Projeto/PrimeiroProjetoApplication.java
@@ -1,14 +1,7 @@
 package com.example.Primeiro_Projeto;
 
-import com.example.Primeiro_Projeto.model.Role;
-import com.example.Primeiro_Projeto.model.User;
-import com.example.Primeiro_Projeto.repositories.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PrimeiroProjetoApplication {
@@ -17,20 +10,4 @@ public class PrimeiroProjetoApplication {
 		SpringApplication.run(PrimeiroProjetoApplication.class, args);
 	}
 
-    @Bean
-    CommandLineRunner init(UserRepository userRepository) {
-        return args -> {
-            Logger log = LoggerFactory.getLogger(PrimeiroProjetoApplication.class);
-
-            log.info("Criando 50 users automaticamente");
-
-            for(int i = 1; i <= 50; i++) {
-                User user = new User("User " + i, "user" + i + "@email.com");
-                user.getRoles().add(Role.USER);
-                userRepository.save(user);
-            }
-
-            log.info("Users criados");
-        };
-    }
 }
